45b0258a98c6c5c22309781f4033e467ae4bdac7
Bjartur Fannar <bjarturfannar@Bjarturs-MacBook-Pro-2.local>
2017-05-03 11:29:21 +0000

    project added

---
+++ CodingDocs/CodingDocs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodingDocs.Startup))]
+namespace CodingDocs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
