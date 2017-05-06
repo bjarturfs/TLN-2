b75de0544414356566e8aee73022fe1a1d5517ca
Bjartur Fannar <bjarturfannar@Bjarturs-MacBook-Pro-2.local>
2017-05-06 16:35:20 +0000

    Added the InviteUserViewModel and InviteUser view

---
+++ CodingDocs/CodingDocs/Models/ViewModels/InviteUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodingDocs.Models.ViewModels
+{
+    public class InviteUserViewModel
+    {
+        public string Name { get; set;  }
+    }
+}
