06f75a81a88df4cf97f30e8f8fab6f29e284b97d
Bjartur Fannar <bjarturfannar@Bjarturs-MacBook-Pro-2.local>
2017-05-10 14:43:30 +0000

    fixed conflict

--- CodingDocs/CodingDocs/Utilities/CustomHandleErrorAttribute.cs
+++ CodingDocs/CodingDocs/Utilities/CustomHandleErrorAttribute.cs
@@ -10,21 +10,21 @@ namespace CodingDocs.Utilities
     {
         public override void OnException(ExceptionContext filterContext)
         {
-            //Get the exception
+            // Get the exception
             Exception ex = filterContext.Exception;
 
-            //TODO: Log the exception!
-            //Example using singleton logger class in Utilities folder which write exception to file
+            // Log the exception!
+            // Example using singleton logger class in Utilities folder which write exception to file
             Logger.Instance.LogException(ex);
 
-            //Set the view name to be returned, maybe return different error view for different exception types
+            // Set the view name to be returned, maybe return different error view for different exception types
             string viewName = "Error";
 
-            //Get current controller and action
+            // Get current controller and action
             string currentController = (string)filterContext.RouteData.Values["controller"];
             string currentActionName = (string)filterContext.RouteData.Values["action"];
 
-            //Create the error model information
+            // Create the error model information
             HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, currentController, currentActionName);
             ViewResult result = new ViewResult
             {
